6872b59237d547e79183b33de643110c12faddfa
sumit0154 <64954113+sumit0154@users.noreply.github.com>
2021-05-20 23:23:47 +0530

    Delete project1/app1 directory

--- project1/app1/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Ocr
-
-admin.site.register(Ocr)
--- project1/app1/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Ocr
-
-
-class ImageUpload(forms.ModelForm):
-    class Meta:
-        model = Ocr
-        fields = ['image']
